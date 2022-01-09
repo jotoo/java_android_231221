c548248450841b47180b159255acd807b46b91b9
jotoo <odintmn2042@yandex.ru>
2022-01-09 13:12:37 +0500

    XOgamerepidFinal

--- src/Lesson4/XOgamerepid.java
+++ src/Lesson4/XOgamerepid.java
@@ -8,7 +8,7 @@ public class XOgamerepid {
     static final Scanner sc = new Scanner(System.in);
     static final Random random = new Random();
 
-    static final int SIZE = 3;
+    static final int SIZE = 5;
     static final int DOTS_TO_WIN = 4;
 
     static final char DOT_X = 'X';
@@ -23,7 +23,7 @@ public class XOgamerepid {
         while (true) {
             humanTurn();
             printMap();
-            if (checkWin(DOT_X)) {
+            if (checkWinLines(DOT_X, DOTS_TO_WIN)) {
                 System.out.println("Вы победили!");
                 break;
             }
@@ -31,10 +31,13 @@ public class XOgamerepid {
                 System.out.println("Ничья!");
                 break;
             }
-
+            aiTurnWin_1();
+            aiTurnDef_1();
+            aiTurnWin_2();
+            aiTurnDef_2();
             aiTurnRandom();
             printMap();
-            if (checkWin(DOT_O)) {
+            if (checkWinLines(DOT_O, DOTS_TO_WIN)) {
                 System.out.println("Компьютер победил!");
                 break;
             }
@@ -80,6 +83,64 @@ public class XOgamerepid {
         map[y][x] = DOT_X;
     }
 
+    public static void aiTurnWin_1() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (isCellValid(i, j)) {
+                    map[i][j] = DOT_O;
+                    if (checkWinLines(DOT_O, DOTS_TO_WIN)) {
+                        return;
+                    }
+                    map[i][j] = DOT_EMPTY;
+                }
+            }
+        }
+    }
+
+    public static void aiTurnDef_1() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (isCellValid(i, j)) {
+                    map[i][j] = DOT_X;
+                    if (checkWinLines(DOT_X, DOTS_TO_WIN)) {
+                        map[i][j] = DOT_O;
+                        return;
+                    }
+                    map[i][j] = DOT_EMPTY;
+                }
+            }
+        }
+    }
+
+    public static void aiTurnWin_2() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (isCellValid(i, j)) {
+                    map[i][j] = DOT_O;
+                    if (checkWinLines(DOT_O, DOTS_TO_WIN) && Math.random() < 0.5) {
+                        return;
+                    }
+                    map[i][j] = DOT_EMPTY;
+                }
+            }
+        }
+    }
+
+    public static void aiTurnDef_2() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (isCellValid(i, j)) {
+                    map[i][j] = DOT_X;
+                    if (checkWinLines(DOT_X, DOTS_TO_WIN) && Math.random() < 0.5) {
+                        map[i][j] = DOT_O;
+                        return;
+                    }
+                    map[i][j] = DOT_EMPTY;
+                }
+            }
+        }
+    }
+
     public static void aiTurnRandom() {
         int y;
         int x;
@@ -108,13 +169,39 @@ public class XOgamerepid {
         return true;
     }
 
-    public static boolean checkWin(char c) {
-        for (int i = 0; i < SIZE; i++) {
-            if ((map[0][i] == c && map[1][i] == c && map[2][i] == c) || (map[i][0] == c && map[i][1] == c && map[i][2] == c)) {
-                return true;
+    /* public static boolean checkWin(char c) {
+         for (int i = 0; i < SIZE; i++) {
+             if ((map[0][i] == c && map[1][i] == c && map[2][i] == c) || (map[i][0] == c && map[i][1] == c && map[i][2] == c)) {
+                 return true;
+             }
+             if ((map[0][0] == c && map[1][1] == c && map[2][2] == c) || (map[0][2] == c && map[1][1] == c && map[2][0] == c)) {
+                 return true;
+             }
+         }
+         return false;
+     }*/
+    public static boolean checkLine(int cy, int cx, int vy, int vx, char dot, int dotsToWin) {
+        if (cx + vx * (dotsToWin - 1) > SIZE - 1 || cy + vy * (dotsToWin - 1) > SIZE - 1 || cy + vy * (dotsToWin - 1) < 0) {
+            return false;
+        }
+        for (int i = 0; i < dotsToWin; i++) {
+            if (map[cy + i * vy][cx + i * vx] != dot) {
+                return false;
             }
-            if ((map[0][0] == c && map[1][1] == c && map[2][2] == c) || (map[0][2] == c && map[1][1] == c && map[2][0] == c)) {
-                return true;
+        }
+        return true;
+    }
+
+    public static boolean checkWinLines(char dot, int dotsToWin) {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (checkLine(i, j, 0, 1, dot, dotsToWin) ||
+                        checkLine(i, j, 1, 0, dot, dotsToWin) ||
+                        checkLine(i, j, 1, 1, dot, dotsToWin) ||
+                        checkLine(i, j, -1, 1, dot, dotsToWin)
+                ) {
+                    return true;
+                }
             }
         }
         return false;
