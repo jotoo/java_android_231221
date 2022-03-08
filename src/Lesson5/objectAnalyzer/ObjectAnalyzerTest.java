b01e48d45229e82f6a71dde72c27d03e04669a06
jotoo <odintmn2042@yandex.ru>
2022-03-08 16:51:20 +0500

    reflection_toString

---
+++ src/Lesson5/objectAnalyzer/ObjectAnalyzerTest.java
@@ -0,0 +1,16 @@
+package Lesson5.objectAnalyzer;
+
+import java.util.ArrayList;
+
+/**
+ * Эта программа использует рефлексию для слежения за обьектами.
+ */
+public class ObjectAnalyzerTest {
+    public static void main(String[] args) throws ReflectiveOperationException {
+        var squares = new ArrayList<Integer>();
+        for (int i = 0; i < 5; i++) {
+            squares.add(i * i);
+            System.out.println(new ObjectAnalyzer().toString(squares));
+        }
+    }
+}
